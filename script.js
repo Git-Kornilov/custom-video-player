41be8c0d7428becf86967b197ccec6f11dc283bc
Alex Kornilov <kornilov.work@gmail.com>
2023-09-16 20:55:54 +0300

    Custom Video Player

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Play and pause video
+const toggleVideoStatus = function () {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+// Update play/pause icons
+const updatePlayIcon = function () {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+};
+
+// Update timestamp
+const updateTimestamp = function () {
+  //   Get minutes
+  let minutes = Math.floor(video.currentTime / 60);
+  if (minutes < 10) {
+    minutes = "0" + String(minutes);
+  }
+
+  // Get seconds
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds < 10) {
+    seconds = "0" + String(seconds);
+  }
+
+  timestamp.innerHTML = `${minutes}:${seconds}`;
+};
+
+// Update progress bar and timestamp
+const updateProgress = function () {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  updateTimestamp();
+};
+
+// Set video time to progress
+const setVideoProgress = function () {
+  video.currentTime = (+progress.value * video.duration) / 100;
+};
+
+// Stop video
+const stopVideo = function () {
+  video.currentTime = 0;
+  video.pause();
+};
+
+// addEventListener
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
